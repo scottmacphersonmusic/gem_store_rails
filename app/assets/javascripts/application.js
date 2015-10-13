005919c98178aac1e93dafbcbc3caafb77a47b37
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-12 23:33:12 -0700

    Current state of mayham

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree ../../../vendor/assets/javascripts/.
+//= require angular
+//= require angular-rails-templates
+//= require_tree ./templates
 //= require_tree .
