37972dd6f3034d1ce9ecbaa0f6f611b9c6e98e43
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-14 13:58:09 -0700

    Add api endpoint for single product request
    Add JSON.parse() helper method

--- test/integration/product_test.rb
+++ test/integration/product_test.rb
@@ -1,12 +1,22 @@
 require 'test_helper'
 
 class ProductTest < ActionDispatch::IntegrationTest
-  test "get request to '/products' returns json data" do
+  test "get request for all products returns json data" do
     get '/products', {}, { Accept: Mime::JSON }
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
-    products = JSON.parse(response.body, symbolize_names: true)
+
+    products = json(response.body)
     ruby = products[:data].detect { |product| product[:attributes][:name] == 'Ruby' }
     assert_equal 'Red', ruby[:attributes][:color]
+  end
+
+  test "get request for single product returns json data" do
+    get '/products/720244849', {}, { Accept: Mime::JSON }
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    product = json(response.body)
+    assert_equal 'Green', product[:data][:attributes][:color]
   end
 end
