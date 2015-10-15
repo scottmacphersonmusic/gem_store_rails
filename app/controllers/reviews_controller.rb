9e2ca7d69b0d02876b64947fbd97362aac3b946b
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-10-14 18:06:46 -0700

    Implement endpoint for requesting single product review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,9 @@ class ReviewsController < ApplicationController
     reviews = Review.where(product_id: params[:product_id])
     render json: reviews, status: 200
   end
+
+  def show
+    review = Review.find(params[:id])
+    render json: review, status: 200
+  end
 end
--- test/integration/review_test.rb
+++ test/integration/review_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class ReviewTest < ActionDispatch::IntegrationTest
+  setup do
+    @product = products(:ruby)
+  end
+
   test "get request for all reviews of a product returns json data" do
-    product = products(:ruby)
-    get "/products/#{product.id}/reviews"
+    get "/products/#{@product.id}/reviews"
     assert_equal 200, response.status
     assert_equal Mime::JSON, response.content_type
 
     reviews = json(response.body)
     ruby_1 = reviews[:data].detect { |review| review[:attributes][:author] == 'thedude@leboswki.com' }
     assert_equal 4, ruby_1[:attributes][:stars]
+  end
+
+  test "get request for single product review returns json data" do
+    review = reviews(:ruby_1)
+    get "/products/#{@product.id}/reviews/#{review.id}"
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    review = json(response.body)
+    assert_equal 'That gem really tied the room together, man',
+                 review[:data][:attributes][:body]
   end
 end
